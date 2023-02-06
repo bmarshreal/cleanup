1e4f887310a37ed26cfc00adf351a95f1ae57866
bmarshreal <65478575+bmarshreal@users.noreply.github.com>
2023-02-06 15:26:05 -0600

    Add files via upload

---
+++ full_cleanup_proj/full_cleanup_1.1.ps1
@@ -0,0 +1,58 @@
+﻿try{
+
+    #9 **Passed Test**
+
+    "tzutil /s 'Central Standard Time'"
+
+    Write-Host -ForegroundColor Green `n"The Following Action has Successfully Completed: ....Time Zone updated to 'Central Standard Time' ."`n
+         
+}catch{
+
+    Throw "A fatal error has occurred.[ --Could not update Time Zone settings.-- #9 ]"
+
+}
+
+Start-Sleep -s 6
+
+try{
+
+    #10
+
+    $newPCName = Read-Host -AsSecureString "Please enter the new name that you would like to label this computer.... Example: SAXXXX "
+
+    Rename-Computer -NewName $newPCName
+    Add-Computer -DomainName labs.ppdi.local
+
+    $pcname = $env:computername
+    $hostmessage = (-join("The Following Action has Successfully Completed: ... The computer has been renamed to ",$pcname,"."))
+
+    Write-Host -ForegroundColor Yellow `n"DO NOT RESTART THIS COMPUTER YET! PLEASE IGNORE ALL "RESTART NOW" PROMPTS.`n WHEN THE SCRIPT HAS FINISHED, IT WILL RESTART THIS COMPUTER."`n
+
+    Write-Host -ForegroundColor Green `n $hostmessage`n
+         
+}catch{
+
+    Throw "A fatal error has occurred.[ --Could not Rename the PC and/or add it to the domain.-- #10 ]"
+
+}
+
+Start-Sleep -s 6
+
+try{
+
+    #11
+
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0 -Force
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+    shutdown -r -t 5
+
+    Write-Host -ForegroundColor Green `n"The Following Action has Successfully Completed: ...This computer will now restart. After restart, please launch the full_cleanup 1.2 script...  "`n
+         
+}catch{
+
+    Throw "A fatal error has occurred.[ --Could not enable Remote Desktop Portal.-- #11 ]"
+
+}
+
+Start-Sleep -s 6
